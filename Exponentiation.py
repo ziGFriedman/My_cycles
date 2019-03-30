ec46e4691dd4414afce642bfd819deceb1152b9d
Nikolay Strizhak <48862178+ziGFriedman@users.noreply.github.com>
2019-03-30 09:10:20 +0500

    Update Exponentiation.py

--- Exponentiation.py
+++ Exponentiation.py
@@ -2,12 +2,12 @@
 def pow(a: float, n: int):
     if n == 0:
         return 1
-    elif n % 2 == 1:    # нечёт
+    elif n % 2 == 1:    # n - нечётное
         return pow(a, n - 1) * a
     else:   # n - чётное
         return pow(a ** 2, n // 2)
 
-a = int(input("Введите число: "))
+a = float(input("Введите число: "))
 n = int(input("Введите степень числа: "))
 
 print(pow(a, n))
