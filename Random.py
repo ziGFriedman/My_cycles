e7256a0c534a8839d87fac7af4f3d61031e974c1
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-04-07 15:40:18 +0500

    Update Random.py

--- Random.py
+++ Random.py
@@ -3,3 +3,7 @@ import random
 
 for i in range(10):
     print(random.randint(1, 100)) # от 1 до 100 включительно
+
+from random import randint      # * - если нужно импортировать весь модуль
+for i in range(10):
+    print(randint(1,10), end=' ' )
