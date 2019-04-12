03ce61aabb526a48574b52d4f6d5f1abaf7d650f
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-04-12 16:10:47 +0500

    Create Binary_array_search.py

---
+++ Binary_array_search.py
@@ -0,0 +1,12 @@
+'''Бинарный поиск в отсортированном массиве'''
+def left_borrd(A, key):
+    left = -1
+    right = len(A)
+    while right - left > 1:
+        middle = (left + right) // 2
+        if A[middle] < key:
+            left = middle
+        else:
+            right = middle
+    return left
+    
