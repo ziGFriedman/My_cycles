fb5698c4be57b9bc4fc836318f640b981d4a4573
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-04-12 13:28:24 +0500

    Update Sort_Tony_Hoara.py

--- Sort_Tony_Hoara.py
+++ Sort_Tony_Hoara.py
@@ -23,8 +23,12 @@ def hoar_sort(A):
         A[k] = x
         k += 1
 
-def check_sorted(A, ascending = True):
 '''Проверка отсортированности за O(len(A))'''
+def check_sorted(A, ascending = True):
     flag = True
+    s = 2 * int(ascending) - 1
     for i in range(0, N-1):
-        if A
+        if s * A[i] > s * A[i+1]:
+            flag = False
+            break
+    return flag
