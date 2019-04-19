bf8e7b27b91d4dc934c2278e635a68e7b31caef8
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-04-19 15:46:07 +0500

    Update Sort_Tony_Hoara.py

--- Sort_Tony_Hoara.py
+++ Sort_Tony_Hoara.py
@@ -27,8 +27,8 @@ def hoar_sort(A):
 def check_sorted(A, ascending = True):
     flag = True
     s = 2 * int(ascending) - 1
-    for i in range(0, N-1):
-        if s * A[i] > s * A[i+1]:
+    for i in range(0, N - 1):
+        if s * A[i] > s * A[i + 1]:
             flag = False
             break
     return flag
