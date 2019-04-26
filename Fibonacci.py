0e10dd2405b641e6882145206a7b8e87eeb1fc20
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-04-26 11:51:48 +0500

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,7 +1,7 @@
 '''Показывает число, стоящее под номером n в последовательности фибоначчи (Динамическое программирование)'''
 def fibonachchi(n):
     fib = [0, 1] + [0] * (n - 1)
-    for i in range(2, n+1):
+    for i in range(2, n + 1):
         fib[i] = fib[i - 1] + fib[i - 2]
         #print(fib[i-1], end=', ') - выдаст все числа Фибоначчи
     return fib[n]
@@ -15,10 +15,7 @@ def fib():
         yield a     # выводит a (подобие return)
         a, b = b, a + b
 
-x = fib()
-print(next(x))
-print(next(x))
-print(next(x))
-print(next(x))
-print(next(x))
-print(next(x))
+x = fib()   # объявление генератора
+y = 10
+for i in range(0, y):
+    print(next(x), end = ' ')
