14b83b9eaac41c48213fdb949989d756dcbcc5cb
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-05-01 19:12:58 +0500

    Update Sum_and_product_of_the_number.py

--- Sum_and_product_of_the_number.py
+++ Sum_and_product_of_the_number.py
@@ -3,11 +3,14 @@ def sum_product (a):
     sum = 0
     mult = 1
     a = abs(a)
-    while a > 0:
-        digit = a % 10
-        sum += digit
-        mult *= digit
-        a = a // 10
-    print('Сумма:', sum, 'Произведение:', mult)
+    if a == 0:
+        print('Сумма:', sum, 'Произведение:', 0)
+    else:
+        while a > 0:
+            digit = a % 10
+            sum += digit
+            mult *= digit
+            a = a // 10
+        print('Сумма:', sum, 'Произведение:', mult)
 
 sum_product(45)
