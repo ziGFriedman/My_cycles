511924e733d20e7a4af708da5830693fb5509305
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-05-01 19:00:40 +0500

    Create Sum_and_product_of_the_number.py

---
+++ Sum_and_product_of_the_number.py
@@ -0,0 +1,12 @@
+'''Находит сумму и произведение цифр числа'''
+def sum_product (a):
+    sum = 0
+    mult = 1
+    while a > 0:
+        digit = a % 10
+        sum += digit
+        mult *= digit
+        a = a // 10
+    print('Сумма:', sum, 'Произведение:', mult)
+
+sum_product(45)
