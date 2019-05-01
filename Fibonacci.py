f8fcb6b98ef5fd70b770b4e526ef873efb84b2c1
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-05-01 19:29:17 +0500

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -3,11 +3,13 @@ def fibonachchi(n):
     fib = [0, 1] + [0] * (n - 1)
     for i in range(2, n + 1):
         fib[i] = fib[i - 1] + fib[i - 2]
-        #print(fib[i-1], end=', ') - выдаст все числа Фибоначчи
+        #print(fib[i-2], end=', ') #- выдаст все числа Фибоначчи
     return fib[n]
 
 print(fibonachchi(10))
 
+
+'''Выводит ряд Фибоначчи'''
 def fib():
     '''генератор'''
     a, b = 0, 1
@@ -16,6 +18,6 @@ def fib():
         a, b = b, a + b
 
 x = fib()   # объявление генератора
-y = 10
+y = 11
 for i in range(0, y):
     print(next(x), end = ' ')
