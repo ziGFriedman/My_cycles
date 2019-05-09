94e3ec07f9eedfa3d7cd15c2f14c9e80ebd14309
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-05-09 18:08:22 +0500

    Update Json_example.py

--- Json_example.py
+++ Json_example.py
@@ -5,19 +5,20 @@ def way_better(filename):
     with open(filename) as f:
         return f.read()
 
- if __name__ == '__main__':
-     data = way_better('data.json')
-     print('raw data is', data, type(data))
-     print()
+if __name__ == '__main__':
+    data = way_better('data.json')
+    print('raw data is', data, type(data))
+    print()
 
-     # From string to object:
-     obj = json.loads(data)
-     print(obj, type(obj))
+    # From string to object:
+    obj = json.loads(data)     # из json в python словарь
+    print(obj, type(obj))
 
-     print(obj['object'], obj['boolean'])
-     print()
+    print(obj['object'], obj['boolean'])     # смотрим что лежит по ключу object и boolean
+    print()
 
-     # From object to string:
-     print('dumping object to text:')
-     obj['new-value'] = 'secret'
-     print(json.dumps(obj))
+    # From object to string:
+    print('dumping object to text:')
+    obj['new-value'] = 'secret'
+    print(json.dumps(obj, sort_keys = True, indent = 4))     # запись нового значения из python в текст вида json, sort_keys - форматирование кода, indent - отступ
+    
