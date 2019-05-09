8989afb3f5d6f62c506c0b3d8463cb718591388c
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-05-09 17:46:10 +0500

    Create Java_script_object_notation.py

---
+++ Java_script_object_notation.py
@@ -0,0 +1,26 @@
+'''Способ предствить объект в виде текста в json'''
+# Является валидным JS-кодом
+# Типы данных в json:
+# string, number, object, array, boolean, null(None)
+
+# string
+{ "name":"John" }
+
+# number
+{ "age":30 }
+
+# object
+{
+"employee":{ "name":"John", "age":30, "city":"New York" }
+}
+
+# array
+{
+"employees":[ "John", "Anna", "Peter" ]
+}
+
+# boolean
+{ "sale":true }
+
+# null
+{ "middlename":null }
