ffdf411d673c5a4575f3416ba036eb49a7f4c16d
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-05-09 17:55:26 +0500

    Create Json_example.py

---
+++ Json_example.py
@@ -0,0 +1,23 @@
+import json
+
+def way_better(filename):
+    print('reading file with way_better()')
+    with open(filename) as f:
+        return f.read()
+
+ if __name__ == '__main__':
+     data = way_better('data.json')
+     print('raw data is', data, type(data))
+     print()
+
+     # From string to object:
+     obj = json.loads(data)
+     print(obj, type(obj))
+
+     print(obj['object'], obj['boolean'])
+     print()
+
+     # From object to string:
+     print('dumping object to text:')
+     obj['new-value'] = 'secret'
+     print(json.dumps(obj))
