5ef466931498ab563a577d6668c32820f4fddba1
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-05-09 10:32:41 +0500

    Create Generators.py

---
+++ Generators.py
@@ -0,0 +1,27 @@
+'''Пример использования генераторов с разными функциями'''
+import random
+
+def gen():
+    while True:
+        yield random.random()
+
+def range_my(start, stop, step):
+    i = start
+    while i < stop:
+        yield i
+        i += step
+
+def map_my(func, my_list):
+    for i in my_list:
+        yield func(i)
+
+def enumerate_my(list_my):
+    i = 0
+    for l in list_my:
+        yield (i, l)
+        i += 1
+
+def my_zip(l1, l2):
+    l_len = len(l1)
+    for i in range(l_len):
+        yield (l1[i], l2[i])
