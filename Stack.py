b28947410ef77e1796e8c75810d529360ded456e
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-06-17 13:58:56 +0500

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -1,19 +1,38 @@
 '''Модуль, описывающий структуру данных - стек
->>>clear()
->>>is_empty()
-True
->>>push(1)
->>>push(2)
->>>push(3)
->>>is_empty()
-False
->>>pop()
-3
->>>pop()
-2
->>>pop()
-1
->>>is_empty()
-True
+# >>>clear()
+# >>>is_empty()
+# True
+# >>>push(1)
+# >>>push(2)
+# >>>push(3)
+# >>>is_empty()
+# False
+# >>>pop()
+# 3
+# >>>pop()
+# 2
+# >>>pop()
+# 1
+# >>>is_empty()
+# True
 '''
-def 
+
+_stack = []
+
+def push(x):
+    '''Добавляет элемент х в уоней сека'''
+    _stack.append(x)
+
+def pop():
+    x = _stack.pop()
+    return x
+
+def clear():
+    _stack.clear()
+
+def is_empty():
+    return len(_stack) == 0
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
