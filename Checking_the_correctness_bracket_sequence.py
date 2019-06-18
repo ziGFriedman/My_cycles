b4cc99551ede1e96c50a93e8c33b537f763e4869
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-06-18 17:51:38 +0500

    Update Checking_the_correctness_bracket_sequence.py

--- Checking_the_correctness_bracket_sequence.py
+++ Checking_the_correctness_bracket_sequence.py
@@ -10,7 +10,7 @@
 # Стек пуст
 import A_stack
 
-def is_braces_sequence_correct():
+def is_braces_sequence_correct(s: str):
     '''
     Проверяет корректность скобочной послед-ти из круглых и квадратных скобок
 
@@ -26,8 +26,21 @@ def is_braces_sequence_correct():
     for brace in s:
         if brace not in "()[]":
             continue
-        if brace in "(["
-
+        if brace in "([":
+            A_stack.push(brace)
+        else:
+            assert brace in ")]", "Ожидалась закрывающая скобка: " + str(brace)
+            if A_stack.is_empty():
+                return False
+            left = A_stack.pop()
+            assert left in "([", "Ожидалась открывающая скобка: " + str(brace)
+            if left == "(":
+                right = ")"
+            elif left == "[":
+                right = "]"
+            if right != brace:
+                return False
+    return A_stack.is_empty()
 
 
 if __name__ == '__main__':
