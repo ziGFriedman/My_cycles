9fbe276e58bd39e86c93a75fb8d91f4108beebfd
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-10-22 16:11:50 +0500

    Update Decorator.py

--- Decorator.py
+++ Decorator.py
@@ -1,40 +1,13 @@
-'''Пример написания декоратора'''
-def decorator(func):
-    func.counter = 1
-
-    def fake(value):
-        print('Runs:', func.counter)
-        func.counter += 1
-        return func(value)
-
-    return fake    # Если функция возвращает функцию, то это декоратор
-
-@decorator    # связь с декоратором (my_str = decorator(my_str))
-def my_str(value):
-    return str(value)
-
-print(my_str(123))
-print(my_str([]))
-print(my_str({}))
-
-
-'''Выводит время выполнения функции'''
-import timeit
-
-def decorator2(func):
-
-    def fake(value):
-        start_time = timeit.default_timer()
-        result = func(value)
-        print('Time is:', timeit.default_timer() - start_time)
+def upper(func):
+    def inner(x,y):
+        print('Result:', x, '+', y)
+        result = func(x,y)
         return result
+    return inner
 
-    return fake
+@upper
+def sum(x,y):
+    print('---')
+    return x+y
 
-@decorator2
-def my_str2(value):
-    return str(value)
-
-print(my_str2(123))
-print(my_str2([]))
-print(my_str2({}))
+print(sum(2, 2))
